8b1ce8709c8493b79759ae8999c44b89cca3fb60
mikeoldmonk <mikeoldmonk@gmail.com>
2022-06-24 19:12:39 +0300

    2 xml, 2 kt

---
+++ app/src/main/java/com/example/tetris/GameActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import com.example.tetris.storage.AppPreferences
+
+class GameActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+    var tvCurrentScore: TextView? = null
+    var appPreferences: AppPreferences? = null
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        appPreferences = AppPreferences(this)
+
+        val btnRestart = findViewById<Button>(R.id.btn_restart)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+        tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+        updateHighScore()
+        updateCurrentScore()
+    }
+
+    private fun updateHighScore() {
+        tvHighScore?.text = "${appPreferences?.getHighScore()}"
+    }
+
+    private fun updateCurrentScore() {
+        tvCurrentScore?.text = "0"
+    }
+}
